82aa77a6972c75ce559470ff4f112df95c4e328f
mbrito <britomauricio2003@gmail.com>
2024-08-16 03:28:36 -0300

    feat: create save button functionality

--- app/src/main/java/com/github/mbrito/cadastro/MainActivity.kt
+++ app/src/main/java/com/github/mbrito/cadastro/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.github.mbrito.cadastro
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.github.mbrito.cadastro.databinding.ActivityMainBinding
@@ -15,12 +16,30 @@ class MainActivity : AppCompatActivity() {
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(activityMainBinding.root)
 
+        activityMainBinding.saveBt.setOnClickListener {
+            val sexo : String = if(activityMainBinding.mascRb.isChecked){
+                "Masculino"
+            }else{
+                "Feminino"
+            }
+            val f = Formulario(
+                activityMainBinding.nameEt.text.toString(),
+                activityMainBinding.emailEt.text.toString(),
+                activityMainBinding.phoneEt.text.toString(),
+                activityMainBinding.cityEt.text.toString(),
+                activityMainBinding.stateSn.selectedItem.toString(),
+                sexo,
+                activityMainBinding.joinEmailsCb.isChecked,
+            )
+            Toast.makeText(this@MainActivity, f.toString(), Toast.LENGTH_LONG).show()
+        }
+
         activityMainBinding.cleanBt.setOnClickListener{
             activityMainBinding.nameEt.text.clear()
             activityMainBinding.emailEt.text.clear()
             activityMainBinding.phoneEt.text.clear()
             activityMainBinding.cityEt.text.clear()
-            activityMainBinding.ticketEmailsCb.isChecked = false
+            activityMainBinding.joinEmailsCb.isChecked = false
             activityMainBinding.mascRb.isChecked = false
             activityMainBinding.femRb.isChecked = false
             activityMainBinding.stateSn.setSelection(0)
