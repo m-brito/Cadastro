058c63af003bf660143c182daf9c7da46c5c4416
mbrito <britomauricio2003@gmail.com>
2024-08-16 02:47:57 -0300

    feat: create 'Formulario' class

---
+++ app/src/main/java/com/github/mbrito/cadastro/Formulario.kt
@@ -0,0 +1,21 @@
+package com.github.mbrito.cadastro
+
+class Formulario(
+    var name: String,
+    var email: String,
+    var phone: String,
+    var city: String,
+    var state: String?,
+    var sexo: String,
+    var join: Boolean
+) {
+    override fun toString(): String {
+        return  "Nome completo: $name\n" +
+                "Email: $email\n" +
+                "Telefone: $phone\n" +
+                "Cidade: $city\n" +
+                "Estado: $state\n" +
+                "Sexo: $sexo\n" +
+                "Ingressar nos emails: $join"
+    }
+}
